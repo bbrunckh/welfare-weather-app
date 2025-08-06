eb4921958487c8a0ffd5543b15b9c6052806537b
Ben Brunckhorst <84766772+bbrunckh@users.noreply.github.com>
2025-08-06 09:46:07 +0100

    update data prep

--- prep/prep_h3.R
+++ prep/prep_h3.R
@@ -1,15 +1,15 @@
 # Prep survey location (H3) data for wise-app
+rm(list = ls())
 
 # Load libraries
+library(pins)
+library(dlw)
+options(dlw.local_dir = "~/dlw/")
 library(duckdbfs)
 load_h3()
 load_spatial()
 library(dplyr)
-library(haven)
-library(pins)
-library(nanoparquet)
 
-# library(dlw)
 setwd("../app/")
 
 #------------------------------------------------------------------------------#
@@ -18,47 +18,46 @@ setwd("../app/")
 # Local folder
 board_local <- board_folder("../app/data/pins")
 
-# Posit - external
-board_posit <- board_connect(server = "external-server")
+# Posit - internal
+board_posit <- board_connect(auth = "envvar")
 
-# # Posit - internal
-# board_posit <- board_connect(server = "internal-server")
+# Posit - external
+# board_posit <- board_connect(server = "external-server")
+
+# overwrite existing data 
+overwrite <- TRUE
 
 #----------------------------------------------------------------------------#
 # Location data for WISE-APP
 
-# Get list of countries with H3 survey data
-loc_path <- paste0("~/Library/CloudStorage/OneDrive-WBG/",
-                   "Household survey locations to H3/LOC/")
-
-h3_list <- list.files(loc_path, recursive = TRUE, "H3.dta$") 
-
-surveys <- pin_read(board_local, "surveys")$wiseapp_pin |> unique()
+# Get list of countries with processed survey data
+surveys <- pin_read(board_local, "surveys")
 
 #------------------------------------------------------------------------------#
 # Loop over surveys 
 
 # loop over H3 files
-for (n in 1:length(h3_list)){
+for (n in 1:nrow(surveys)){
   
-  pin_name <- substring(h3_list[n], 5, 12)
+  pin_name <- surveys$wiseapp_pin[n]
   print(pin_name)
   
-  # skip if no survey data
-  if(!pin_name %in% surveys){next}
+  code <- substr(pin_name, 1, 3)
+  year <- substr(pin_name, 5, 8)
   
-  # skip if file exists
-  # if(file.exists(paste0("data/surveys/",code,"_", year, "_H3.parquet")) & 
-  #    file.exists(paste0("data/surveys/",code,"_", year, "_LOC.parquet"))){next}
+  # fname <- substr(surveys$gmd_spat[n], 1, nchar(surveys$gmd_spat[n]) - 8)
   
-  h3 <- read_dta(paste0(loc_path,h3_list[n]), encoding = "latin1") 
+  # skip if pins exist
+  if (!overwrite) {
+    if (pin_exists(board_local, paste0(pin_name,"_H3")) & 
+        pin_exists(board_local, paste0(pin_name,"_LOC"))) {next}
+  }
   
-    # h3 <- dlw_get_gmd(code, year, "H3") # from datalibweb
+  # h3 <- dlw_get_gmd(code, year, "H3")
   
-  # save H3 level parquet data to disk
-  write_parquet(h3, 
-                paste0("data/surveys/",pin_name, "_H3.parquet"),
-                options = parquet_options(write_minmax_values = FALSE))
+  h3 <- haven::read_dta(
+    list.files('/Users/bbrunckhorst/Library/CloudStorage/OneDrive-WBG/Household survey locations to H3/LOC',
+               paste0(pin_name,".+_H3.dta$"), recursive = TRUE, full.names = TRUE))
   
   # Pin H3 level parquet data to Posit Connect board for app
   pin_write(board_local, h3, paste0(pin_name,"_H3"), type = "parquet")
@@ -70,23 +69,23 @@ for (n in 1:length(h3_list)){
     distinct(loc_id, urban, int_date) |>
     summarise(int_dates = str_flatten(int_date, ", "), .by = c(loc_id, urban))
     
-
   # Aggregate H3 hexagon boundaries to spatial units (loc_id)
+  
+  if (code != "FJI"){
   loc_geo <- as_dataset(h3) |>
     mutate(geom = st_geomfromtext(h3_cell_to_boundary_wkt(h3_7))) |> 
     summarise(geom = ST_AsText(st_union_agg(geom)), 
               .by = c(code, year, loc_id)) |> 
     left_join(loc_dates) |>
     collect()
-
-  # save loc_id level parquet data to disk
-  write_parquet(loc_geo, 
-                paste0("data/surveys/",pin_name, "_LOC.parquet"),
-                options = parquet_options(write_minmax_values = FALSE))
   
   # Pin loc_id level parquet data to Posit Connect board for app
   pin_write(board_local, loc_geo, paste0(pin_name,"_LOC"), type = "parquet")
   pin_write(board_posit, loc_geo, paste0(pin_name,"_LOC"), type = "parquet")
+  }
+  
+  rm(h3, loc_dates, loc_geo)
+  close_connection()
   
 }
 #------------------------------------------------------------------------------#
