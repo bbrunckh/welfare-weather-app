4f3525d916d31e30e4270bdd481cf821ebbfd51b
dr9593 <kryger.daniel@gmail.com>
2026-01-28 15:02:08 -0500

    get current version working on drk local

---
+++ R/drk_prep_code.R
@@ -0,0 +1,6 @@
+#DRK Code to get 
+getwd() #make sure ‘welfare-weather-app’
+setwd(""C:/Users/wb617223/OneDrive - WBG/Documents/welfare-weather-app"")
+devtools::document()
+devtools::load_all()
+wiseapp::run_app()
